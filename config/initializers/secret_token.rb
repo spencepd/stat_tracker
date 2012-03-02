af27d75ce3ded3510fe5ff7b2297b08ca6ec473f
Paul Spencer <spencepd@gmail.com>
2012-03-01 21:28:49 -0500

    just started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StatTracker::Application.config.secret_token = '038f41342456ed6b5cca4f172c697cabdddd72d31a0ccd02fb64db9c923d8e8f212406723c15cf7ad9f920d7375de014c6ad5c54a06675afcdf0e6951b83475f'
